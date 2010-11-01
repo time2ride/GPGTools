de384d8c4c0fd83026806e90045fc71a93390f82
Felix Co <ichentwickle@arcor.de>
2010-11-01 15:28:39 +0100

    check for gpg or gpg2 should now work

--- payload/check_for_gpg.sh
+++ payload/check_for_gpg.sh
@@ -2,10 +2,11 @@
 
 # this skript returns 0 if either gpg or gpg2 is installed on the system
 
-if( which -s gpg ) then
+PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin
+if( /usr/bin/which -s gpg ) then
 	exit 0;
 fi
-if( which -s gpg2 ) then
+if( /usr/bin/which -s gpg2 ) then
 	exit 0;
 fi
 exit 1;
